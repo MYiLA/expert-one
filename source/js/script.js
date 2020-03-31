c1299329ee23a3d9c30d248a78461be0ee6f0f60
MYiLA <polus180@ya.ru>
2020-04-01 00:48:58 +0300

    header created

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+// кастомные селекты
+
+const choiceLanguage = new Choices('.expert-one .menu__language', {
+  searchChoices: false,
+  shouldSort: false,
+  sorter: function() {
+   return false
+ }
+});
+
+//открыть/закрыть меню
+
+$('.expert-one .header__burger').on('click', function () {
+  $('.expert-one .header').toggleClass('header--open');
+})
