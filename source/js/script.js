cf3c53d502a2c5ff59ba2cc511ef0beb94b9064b
MYiLA <polus180@ya.ru>
2020-04-02 10:59:01 +0300

    filter created

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 // кастомные селекты
 
-const choiceLanguage = new Choices('.expert-one .menu__language', {
+const choiceLang = new Choices('.expert-one .menu__lang', {
   searchChoices: false,
   shouldSort: false,
   sorter: function() {
@@ -9,8 +9,37 @@ const choiceLanguage = new Choices('.expert-one .menu__language', {
  }
 });
 
+const choiceFilterLang = new Choices('.expert-one .filter__select--lang', {
+  searchChoices: false,
+  shouldSort: true,
+  sorter: function() {
+   return true
+ }
+});
+
+const choiceFilterCountry = new Choices('.expert-one .filter__select--country', {
+  searchChoices: false,
+  shouldSort: true,
+  sorter: function() {
+   return true
+ }
+});
+
 //открыть/закрыть меню
 
 $('.expert-one .header__burger').on('click', function () {
-  $('.expert-one .header').toggleClass('header--open');
+  $('.expert-one .header').toggleClass('active');
+  $('.expert-one').toggleClass('no-scroll');
+})
+
+// открыть/закрыть фильтр
+
+$('.expert-one .filter__open').on('click', function () {
+  $('.expert-one .filter__popup').addClass('active');
+  $('.expert-one').addClass('no-scroll');
+})
+
+$('.expert-one .filter__close').on('click', function () {
+  $('.expert-one .filter__popup').removeClass('active');
+  $('.expert-one').removeClass('no-scroll');
 })
