85b69a89ff05e3e6f9a0b49315008ba770c9510b
MYiLA <polus180@ya.ru>
2020-04-30 21:56:06 +0300

    add input for order-page

---
+++ js/order-page.js
@@ -0,0 +1,1 @@
+"use strict";$(".expert-one .customer-data__btn--edit").on("click",(function(){$(".expert-one .customer-data__wrap-main").addClass("edit")})),$(".expert-one .customer-data__btn--save").on("click",(function(){$(".expert-one .customer-data__wrap-main").removeClass("edit")}));
